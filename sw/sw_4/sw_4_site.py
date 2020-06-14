ac2e102a2e618bf8b7794629ff3b2c3e891f06a9
Marco <manco.mrc@gmail.com>
2020-06-14 23:10:52 +0200

    Add /ventola /led /stufa

--- sw/sw_4/sw_4_site.py
+++ sw/sw_4/sw_4_site.py
@@ -10,12 +10,36 @@ class Site():
         with open("index.html", "r") as f:
             self.index_html = f.read()
 
+        self.led = None
+        self.ventola = None
+        self.stufa = None
         
 
     def GET(self, *uri, **param):
 
         if len(uri) == 0:
             return self.index_html
+
+        if uri[0] == "led":
+            self.led = param.get("subject")
+
+            if self.led == "on":
+                return self.led
+            else: # off
+                return self.led
+
+        if uri[0] == "ventola":
+            self.ventola = param.get("vol")
+
+            return self.ventola
+
+        if uri[0] == "stufa":
+            self.stufa = param.get("vol")
+            
+            return self.stufa
+
+        cp.HTTPError(404, "Page not found")
+
 
 if __name__ == "__main__":
     conf = {
